3fc0300a375acc6df0300e7a6b934992cf2d0b54
Rithish <rithish.k@akshayahgs.org>
2021-06-29 16:59:06 +0530

    Added files

---
+++ table.js
@@ -0,0 +1,40 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const periodicTable = document.getElementById('periodic-Table');
+    const description = document.querySelector('.content');
+    const close  = document.querySelector('.close')
+    const popup = document.querySelector('.description')
+
+    fetch('PeriodicTable.json')
+        .then((response) => {
+            return response.json()
+        })
+        .then((elements) => {
+            elements.forEach((element, i) => {
+                const elem = document.createElement('div');
+                let id = i += 1
+                elem.className = 'element' + ' n' + id + ' ' + element.category;
+                elem.innerHTML = element.atomicNumber + '<h1>' + element.symbol + '</h1>' + '<p>' + element.name + '</p>';
+                periodicTable.appendChild(elem);
+                elem.addEventListener('click', () => {
+                    if(id == element.atomicNumber) {
+                        description.innerHTML = 
+                        `<strong>Atomic-mass: </strong> ${element.atomicMass} <br/> <br/>
+                        <strong>Year discovered: </strong> ${element.discovered} <br/> <br/>
+                        <strong>Appearance: </strong> ${element.appearance} <br/> <br/>
+                        <strong>Melting point: </strong> ${element.meltingPoint} <br/> <br/>
+                        <strong>Boiling point: </strong> ${element.boilingPoint} <br/> <br/>
+                        <strong>Density (at STP): </strong> ${element.density} <br/> <br/>
+                        <strong>State (at STP): </strong> ${element.state} <br/> <br/>
+                        <strong>Category: </strong> ${element.category} <br/> <br/>
+                        <strong>Discovered by: </strong> ${element.discoveredBy} <br/> <br/>
+                        <strong>Description: </strong> <br/> ${element.description}<br/> `
+                        popup.style.display = 'block'
+                    }
+                })
+            });
+        })
+        
+        close.addEventListener('click', () => {
+            popup.style.display = 'none';
+        })
+})
